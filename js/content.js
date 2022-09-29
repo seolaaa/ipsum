cde1d1704af77c6164466d5464b1896d94f5fba5
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-09-29 22:31:29 +0900

    text단 완성

--- js/content.js
+++ js/content.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
-    const proContentLi = document.querySelectorAll('#content-menu>li') 
+    const proContentLi = document.querySelectorAll('#content-menu>li')
+    const textWrap = document.querySelector('#content-text-wrap')
 
 
 
@@ -10,8 +11,10 @@ document.addEventListener('DOMContentLoaded',()=>{
     function initEvent(){
 
         window.addEventListener('scroll',scrollPro)
+        window.addEventListener('scroll',scrollText)
 
     }
+
 
     function scrollPro(){
 
@@ -22,7 +25,7 @@ document.addEventListener('DOMContentLoaded',()=>{
                 gsap.to(proContentLi[i],{top:0,opacity:1,duration:0.4,ease:'power1.out',delay:i*0.2})
             }
         }
-        if(scrollHeight>1700){
+        if(scrollHeight>1600){
             for(let i=0; i<proContentLi.length; i++){
                 gsap.to(proContentLi[i],{top:150,opacity:0,duration:0.4,ease:'power1.out',delay:i*0.2})
             }
@@ -32,5 +35,21 @@ document.addEventListener('DOMContentLoaded',()=>{
             gsap.to(proContentLi[i],{top:0,opacity:1,duration:0.4,ease:'power1.out',delay:i*0.2})
         }
         
+    }
+
+    function scrollText(){
+
+        let scrollHeight = window.pageYOffset;
+
+        if(scrollHeight>1000){
+            gsap.to(textWrap,{display:'block',opacity:1,duration:0.5,ease:'power1.out'})
+        }
+        
+        if(scrollHeight>1800){
+            gsap.to(textWrap,{display:'none',opacity:0,duration:0.5,ease:'power1.out'})  
+        }
+        else{
+            gsap.to(textWrap,{display:'block',opacity:1,duration:0.5,ease:'power1.out'})
+        }
     }
 })
