226458bac54d6dc51aeba4bcfda3ad502dcb57d7
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-09-29 15:35:00 +0900

    메인 js 수정완료
    내리고 올릴때 애니메이션적용

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ document.addEventListener('DOMContentLoaded',()=>{
     const rightText = document.querySelector('.right-text')
     const mainBtn = document.querySelector('.main-btn')
 
+    const mainWrap = document.querySelector('#main-wrap')
 
-    init()
+    init();
+    initEvent();
 
 
     function init(){
@@ -19,6 +21,35 @@ document.addEventListener('DOMContentLoaded',()=>{
         gsap.to(leftText,{opacity:1,left:250,duration:0.6,ease:'power1.out',delay:0.8})
         gsap.to(rightText,{opacity:1,right:255,duration:0.6,ease:'power1.out',delay:0.8})
         gsap.to(mainBtn,{opacity:1,right:150,duration:0.6,ease:'power1.out',delay:0.9})
+
+    }
+
+    function initEvent(){
+
+        window.addEventListener('scroll',scrollMain)
+
+    }
+
+    function scrollMain(){
+        
+        let scrollHeight = window.pageYOffset;
+
+        if(scrollHeight>900){
+            gsap.to(mainImg,{opacity:0,top:350,duration:0.6,ease:'power1.out',delay:0.2})
+            gsap.to(title,{opacity:0,top:240,duration:0.6,ease:'power1.out',delay:0.5})
+            gsap.to(text,{opacity:0,top:450,duration:0.6,ease:'power1.out',delay:0.8})
+            gsap.to(leftText,{opacity:0,left:250,duration:0.6,ease:'power1.out',delay:0.8})
+            gsap.to(rightText,{opacity:0,right:100,duration:0.6,ease:'power1.out',delay:0.8})
+            gsap.to(mainBtn,{opacity:0,right:0,duration:0.6,ease:'power1.out',delay:0.9})
+        }
+        else{
+            gsap.to(mainImg,{opacity:1,top:150,duration:0.6,ease:'power1.out',delay:0.2})
+            gsap.to(title,{opacity:1,top:140,duration:0.6,ease:'power1.out',delay:0.5})
+            gsap.to(text,{opacity:1,top:300,duration:0.6,ease:'power1.out',delay:0.8})
+            gsap.to(leftText,{opacity:1,left:250,duration:0.6,ease:'power1.out',delay:0.8})
+            gsap.to(rightText,{opacity:1,right:255,duration:0.6,ease:'power1.out',delay:0.8})
+            gsap.to(mainBtn,{opacity:1,right:150,duration:0.6,ease:'power1.out',delay:0.9})
+        }
 
     }
 })
