e6922bfd9b95c332e205f3beab0b821b85b8043b
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-09-30 16:07:22 +0900

    9/30 스크롤수정

--- js/content.js
+++ js/content.js
@@ -19,12 +19,12 @@ document.addEventListener('DOMContentLoaded',()=>{
 
         let scrollHeight = window.pageYOffset;
 
-        if(scrollHeight<500){
+        if(scrollHeight>=1200){
             for(let i=0; i<proContentLi.length; i++){
                 gsap.to(proContentLi[i],{top:0,opacity:1,duration:0.4,ease:'power1.out',delay:i*0.2})
             }
         }
-        if(scrollHeight>1600){
+        if(scrollHeight>=1600){
             for(let i=0; i<proContentLi.length; i++){
                 gsap.to(proContentLi[i],{top:150,opacity:0,duration:0.4,ease:'power1.out',delay:i*0.2})
             }
@@ -40,11 +40,11 @@ document.addEventListener('DOMContentLoaded',()=>{
 
         let scrollHeight = window.pageYOffset;
 
-        if(scrollHeight<1700){
+        if(scrollHeight>=1800){
             gsap.to(textWrap,{display:'block',opacity:1,duration:0.5,ease:'power1.out'})
         }
         
-        if(scrollHeight>1800){
+        if(scrollHeight>=1600){
             gsap.to(textWrap,{display:'none',opacity:0,duration:0.5,ease:'power1.out'})  
         }
         else{
