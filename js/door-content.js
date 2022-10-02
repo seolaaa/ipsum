1fa61db9819ca5977550069a609007daeb2fdaf5
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-10-02 15:04:03 +0900

    left-wrap 포지션 / 추가

--- js/door-content.js
+++ js/door-content.js
@@ -3,20 +3,22 @@ document.addEventListener('DOMContentLoaded',()=>{
     const doorWrap = document.querySelector('#door-img-list-wrap')
     const doorImg = document.querySelector('#door-img-list')
 
-    let startX = doorWrap.offsetLeft-(doorImg.offsetWidth/7);
+    let startX = doorWrap.offsetLeft-(doorWrap.offsetWidth);
     let CurrentX = startX;
     let stepX = 2;
     let endX = doorWrap.offsetLeft;
 
+    
 
 
-    Moving();
+
+    // Moving();
 
     
 
     function Moving(){
 
-        setInterval(doorMoving,18)
+        setInterval(doorMoving,20)
     }
 
     function doorMoving(){
@@ -24,7 +26,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         CurrentX+=stepX
 
         if(CurrentX >= endX){
-            CurrentX=startX+5
+            CurrentX=startX+2
         }
 
         doorWrap.style.left=CurrentX+"px"
