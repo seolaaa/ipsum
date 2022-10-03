7121a13cd6ca4c4e96b22e2fa8117a114bacaeec
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2022-10-03 16:36:11 +0900

    door 슬라이드 수정4 - 숫자적용
    숫자도같이적용0

--- js/door-content.js
+++ js/door-content.js
@@ -44,13 +44,17 @@ document.addEventListener('DOMContentLoaded',()=>{
     const doorWrap = document.querySelector('#door-img-list-wrap')
     const doorListWrap = document.querySelector('#door-img-list')
     const doorLi = document.querySelectorAll('#door-img-list>li')
-    const num = document.querySelectorAll('#door-img-num-list>li')
+    const numWrap = document.querySelector('#door-img-num-list')
+    const numLi = document.querySelectorAll('#door-img-num-list>li')
     const leftBtn = document.querySelector('.door-left-btn')
     const rightBtn = document.querySelector('.door-right-btn')
 
 
     let doorLength = doorLi.length;
     let doorWidth = doorListWrap.offsetWidth;
+
+    let numLength = numLi.length;
+    let numWidth = numWrap.offsetWidth;
 
 
     let currentIndex=0;
@@ -67,8 +71,10 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
     function init(){
-        gsap.set(doorLi,{left:doorWidth,opacity:0.2})
+        gsap.set(doorLi,{left:doorWidth,opacity:0})
         gsap.set(doorLi[0],{left:0,opacity:1})
+        gsap.set(numLi,{left:numWidth,opacity:0})
+        gsap.set(numLi[0],{left:0,opacity:1})
     }
 
 
@@ -96,6 +102,12 @@ document.addEventListener('DOMContentLoaded',()=>{
           gsap.to(doorLi[nextIndex],{left:0,opacity:1,duration:0.8,ease:'power1.out',onComplete:()=>{
             isSlide=false;
           }})
+
+          gsap.to(numLi[currentIndex],{left:-numWidth,opacity:0, duration:0.2, ease:'power1.out'})
+          gsap.set(numLi[nextIndex],{left:numWidth})
+          gsap.to(numLi[nextIndex],{left:0,opacity:1,duration:0.2,ease:'power1.out',onComplete:()=>{
+            isSlide=false;
+          }})
   
           currentIndex=nextIndex;
   
@@ -114,6 +126,12 @@ document.addEventListener('DOMContentLoaded',()=>{
           gsap.to(doorLi[currentIndex],{left:doorWidth,opacity:0, duration:0.8, ease:'power1.out'})
           gsap.set(doorLi[nextIndex],{left:-doorWidth})
           gsap.to(doorLi[nextIndex],{left:0,opacity:1,duration:0.8,ease:'power1.out',onComplete:()=>{
+            isSlide=false;
+          }})
+
+          gsap.to(numLi[currentIndex],{left:numWidth,opacity:0, duration:0.2, ease:'power1.out'})
+          gsap.set(numLi[nextIndex],{left:-numWidth})
+          gsap.to(numLi[nextIndex],{left:0,opacity:1,duration:0.2,ease:'power1.out',onComplete:()=>{
             isSlide=false;
           }})
   
